d81c80991a3b63c76866a2a42505cd083b734676
Vivek <78008481+v-v-r-official@users.noreply.github.com>
2021-06-01 13:01:28 +0530

    Update script.py

--- script.py
+++ script.py
@@ -21,10 +21,10 @@ I am Simple Rename Bot!😌</b>
     
 1. Send me any Telegram File.
 2. Choose appropriate option."""
-    ABOUT_ME = """ <b>Creator:</b> [Vivek](https://t.me/VivekTVP)
-<b>Credits:</b> Dan for his awesome pyrogram library 🔥
-<b>Library:</b> [Pyrogram 1.1.13](https://docs.pyrogram.org/)
-<b>Updates:</b> [vk projects](https://t.me/VkProjects)
-<b>Server:</b> [Heroku](https://heroku.com)"""
+    ABOUT_ME = """ **Creator:** [Vivek](https://t.me/VivekTVP)
+**Credits:** Dan for his awesome pyrogram library 🔥
+**Library:** [Pyrogram 1.1.13](https://docs.pyrogram.org/)
+**Updates:** [vk projects](https://t.me/VkProjects)
+**Server:** [Heroku](https://heroku.com)"""
     FORCE_SUBSCRIBE_TEXT = "<code>Sorry Dear You Must Join My Updates Channel for using me 😌😉....</code>"
     SOMETHING_WRONG = "<code>Something Wrong. Try again.</code>"
