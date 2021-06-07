ca6eae53bffbe717323916ca0ca6530991fc88d5
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-06-07 13:19:00 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,6 +10,44 @@ async def cb_handler(bot, update):
     if "rename_button" in update.data:
         await update.message.delete()
         await force_name(bot, update.message)
+        )
+        return
+
+    elif query.data == "help":
+        await query.answer()
+        keyboard = InlineKeyboardMarkup(
+            [[
+            InlineKeyboardButton("About 🤖", callback_data="about"),
+            InlineKeyboardButton("Close 🔐", callback_data="close")
+            ]]
+         )
+
+        await query.message.edit_text(
+            Script.HELP_MSG,
+            reply_markup=keyboard,
+            disable_web_page_preview=True
+        )
+        return
+
+    elif query.data == "about":
+        await query.answer()
+        keyboard = InlineKeyboardMarkup(
+            [[
+            InlineKeyboardButton("🤔 How To Use", callback_data="help"),
+            InlineKeyboardButton("Close 🔐", callback_data="close")
+            ]]
+         )
+
+        await query.message.edit_text(
+            Script.ABOUT_MSG,
+            reply_markup=keyboard,
+            disable_web_page_preview=True
+        )
+        return
+
+    elif query.data == "close_data":
+        await query.message.delete()
+
         
     elif "cancel_e" in update.data:
         await update.message.delete()
