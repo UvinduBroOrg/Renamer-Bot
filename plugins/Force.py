57060dd5ea4b222652836a70144425155ef1e85b
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-06-08 10:33:37 +0530

    Update Force.py

--- plugins/Force.py
+++ plugins/Force.py
@@ -26,41 +26,45 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from pyrogram.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import UserNotParticipant, UserBannedInChannel 
 
-START_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton(text="🤔 How To Use", callback_data="help"),
-        InlineKeyboardButton(text="🤖 About", callback_data="about"),
-        ],[
-        InlineKeyboardButton(text="Close 🔐", callback_data="close")
-        ]]
-    )
+import logging
+logger = logging.getLogger(__name__)
 
-@Bot.on_message(pyrogram.filters.command(["start"]))
-async def text(bot, update):
-    if update.from_user.id in Config.BANNED_USERS:
-        await update.reply_text("You are Banned")
-        return
-    update_channel = Config.UPDATE_CHANNEL
-    if update_channel:
+import datetime
+from ..config import Config
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors import UserNotParticipant, ChatAdminRequired, UsernameNotOccupied
+
+
+@Client.on_message(filters.private & filters.incoming)
+async def force_sub(c, m):
+    if Config.FORCE_SUB:
         try:
-            user = await bot.get_chat_member(update_channel, update.chat.id)
-            if user.status == "kicked":
-               await update.reply_text(" Sorry, You are **B A N N E D**")
-               return
+            chat = await c.get_chat_member(Config.FORCE_SUB, m.from_user.id)
+            if chat.status=='kicked':
+                return await m.reply_text('Hai you are kicked from my updates channel. So, you are not able to use me',  quote=True)
+
         except UserNotParticipant:
-            #await update.reply_text(f"Join @{update_channel} To Use Me")
-            await update.reply_text(
-                text="**Please Join My Update Channel Before Using Me..**",
-                reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
-              ])
-            )
-            return
-        else:
-            await update.reply_text(script.START_TEXT.format(update.from_user.first_name),
-        reply_markup= START_BUTTONS,
-        parse_mode="html",
-        disable_web_page_preview=True,
-        reply_to_message_id=update.message_id
-    )
-           # return
+            button = [[InlineKeyboardButton('join Updates channel', url='https://t.me/{Config.FORCE_SUB}')]]
+            markup = InlineKeyboardMarkup(button)
+            return await m.reply_text(text="Hey join in my updates channel to use me.", parse_mode='markdown', reply_markup=markup, quote=True)
+
+        except ChatAdminRequired:
+            logger.warning(f"Make me admin in @{Config.FORCE_SUB}")
+            if m.from_user.id in Config.AUTH_USERS:
+                return await m.reply_text(f"Make me admin in @{Config.FORCE_SUB}")
+
+        except UsernameNotOccupied:
+            logger.warning("The forcesub username was Incorrect. Please give the correct username.")
+            if m.from_user.id in Config.AUTH_USERS:
+                return await m.reply_text("The forcesub username was Incorrect. Please give the correct username.")
+
+        except Exception as e:
+            if "belongs to a user" in str(e):
+                logger.warning("Forcesub username must be a channel username Not yours or any other users username")
+                if m.from_user.id in Config.AUTH_USERS:
+                    return await m.reply_text("Forcesub username must be a channel username Not yours or any other users username")
+            logger.error(e)
+            return await m.reply_text("Some thing went wrong. Try again and if same issue occur contact [our group](https://t.me/VKP_BOTS)", disable_web_page_preview=True, quote=True)
+
+    await m.continue_propagation()
