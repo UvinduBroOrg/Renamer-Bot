a641072f71fb8a9f7893c3bd54b150e0790ae21f
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-06-08 11:14:12 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -5,7 +5,6 @@ from plugins.rename_file import force_name
 from pyrogram import Client as pyrogram, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ForceReply
 from pyrogram.errors import UserNotParticipant
-
 from script import Script
 
 helpbutton = [[
